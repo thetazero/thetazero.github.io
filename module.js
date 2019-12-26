01f576021c462ac0e5213add9d94b5149634dfd9
thetazero <aseletskiy@cloudlinux.com>
2019-12-26 14:30:13 -0800

    incomplete test

---
+++ module.js
@@ -0,0 +1,61 @@
+customElements.define('theta-module', class extends HTMLElement {
+  constructor() {
+    super()
+    const shadow = this.attachShadow({ mode: 'open' })
+    console.log(this.title)
+    shadow.innerHTML = `
+    <a class='link'>
+    <span class='text'>
+      <span class='title'></span><br>
+      <span class='desc'></span>
+    </span>
+    <style>
+      .text{
+        position:relative;
+        top:50%;
+        display:inline-block;
+        transform:translateY(-50%);
+      }
+      .title{
+        font-size:2em;
+      }
+      .desc{
+        font-size:0.8em;
+      }
+      @media only screen and (max-width: 768px) {
+        .title{
+          font-size:4em;
+        }
+      }
+      :host{
+        background:var(--background_emphasize);
+        padding:5px;
+        border-radius:5px;
+        cursor:pointer;
+        transition:color 100ms ease-in-out;
+        vertical-align:top;
+      }
+      .link{
+        color:var(--text_emphasize);
+        width:100%;
+        height:100%;
+        display:block;
+      }
+      :host(:hover) .title{
+        color:var(--text_accent);
+      }
+      </a>
+    </style>`
+  }
+  connectedCallback() {
+    this.shadowRoot.querySelector('span.title').innerText = this.title
+    this.shadowRoot.querySelector('span.desc').innerText = this.desc
+    this.shadowRoot.querySelector('a.link').setAttribute('href', this.getAttribute('href'))
+  }
+  get title() {
+    return this.getAttribute('title')
+  }
+  get desc() {
+    return this.getAttribute('desc')
+  }
+})
---
+++ nav.js
@@ -0,0 +1,52 @@
+customElements.define('theta-nav', class extends HTMLElement {
+  constructor() {
+    super()
+    const shadow = this.attachShadow({ mode: 'open' })
+    shadow.innerHTML = `
+    <a class='logo' href='/'>
+      <span>0</span>
+      <span>Θ</span>
+    </div>
+    <style>
+      :host{
+        position:fixed;
+        left:0px;
+        top:0px;
+        width:100%;
+        background:var(--background_nav);
+        --size:50px;
+        height:var(--size);
+        z-index:100;
+      }
+
+      @media only screen and (max-width: 768px) {
+        
+      }
+
+      .logo{
+        width:var(--size);
+        height:var(--size);
+        cursor:pointer;
+        transition:color var(--text_transition_speed);
+        color:var(--text);
+        display:inline-block;
+      }
+
+      span{
+        position:absolute;
+        transform:translate(-50%,-50%);
+        display:inline-block;
+        user-select:none;
+        font-weight:100;
+        color:inherit;
+        font-size:var(--size);
+        top:calc(var(--size) / 2);
+        left:calc(var(--size) / 2);
+        line-height:var(--size);
+      }
+      .logo:hover{
+        color:var(--text_accent) !important;
+      }
+    </style>`
+  }
+})
